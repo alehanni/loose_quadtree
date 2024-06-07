f08887bfe1f20cd409f5dca50307e3198a72c235
alehanni <alehanni@student.chalmers.se>
2024-06-07 20:00:50 +0200

    added region queries

--- include/quadtree.h
+++ include/quadtree.h
@@ -1,102 +1,233 @@
 #ifndef QUADTREE_H
 #define QUADTREE_H
 
+#include <vector>
 #include <limits>
 #include <algorithm>
 #include <cstdint>
 #include <cassert>
+#include <raylib.h> // ONLY USED FOR DEMO
+
+using node_id = std::uint32_t;
+static constexpr node_id null = node_id(-1);
+static constexpr float inf = std::numeric_limits<float>::infinity();
 
 struct point {
     float x, y;
 };
 
-static constexpr float inf = std::numeric_limits<float>::infinity();
+struct bbox {
+    float minx = inf;
+    float miny = inf;
+    float maxx = -inf;
+    float maxy = -inf;
 
-struct box {
-    point min{inf, inf};
-    point max{-inf, -inf};
+    void split_4(bbox &bb1, bbox &bb2, bbox &bb3, bbox &bb4) const {
 
-    box &operator |= (point const &p) {
-        min.x = std::min(min.x, p.x);
-        min.y = std::min(min.y, p.y);
-        max.x = std::max(max.x, p.x);
-        max.y = std::max(max.y, p.y);
+        float midx = (minx + maxx) / 2.f;
+        float midy = (miny + maxy) / 2.f;
+
+        bb1.minx = minx;
+        bb1.miny = miny;
+        bb1.maxx = midx;
+        bb1.maxy = midy;
+
+        bb2.minx = midx;
+        bb2.miny = miny;
+        bb2.maxx = maxx;
+        bb2.maxy = midy;
+
+        bb3.minx = minx;
+        bb3.miny = midy;
+        bb3.maxx = midx;
+        bb3.maxy = maxy;
+
+        bb4.minx = midx;
+        bb4.miny = midy;
+        bb4.maxx = maxx;
+        bb4.maxy = maxy;
+    }
+
+    bool intersect(bbox &other) {
+        return (maxx > other.minx && minx < other.maxx
+            && maxy > other.miny && miny < other.maxy);
+    }
+
+    bbox &operator |=(point rhs) {
+        minx = std::min(minx, rhs.x);
+        miny = std::min(miny, rhs.y);
+        maxx = std::max(maxx, rhs.x);
+        maxy = std::max(maxy, rhs.y);
         return *this;
+    }
+
+    friend bbox operator|(bbox lhs, point const& rhs) {
+        lhs |= rhs;
+        return lhs;
+    }
+
+    bbox &operator |=(bbox rhs) {
+        minx = std::min(minx, rhs.minx);
+        miny = std::min(miny, rhs.miny);
+        maxx = std::max(maxx, rhs.maxx);
+        maxy = std::max(maxy, rhs.maxy);
+        return *this;
+    }
+
+    friend bbox operator|(bbox lhs, bbox const& rhs) {
+        lhs |= rhs;
+        return lhs;
     }
 };
 
-point middle(point const &p1, point const &p2) {
-    return {(p1.x + p2.x) / 2.f, (p1.y + p2.y) / 2.f};
-}
+struct pointbox {
+    point mid;
+    bbox bb;
+};
 
-box bbox(point *begin, point *end) {
-    box result;
-    for (auto it = begin; it != end; ++it)
-        result |= *it;
-    return result;
-}
-
-using node_id = std::uint32_t;
-
-static constexpr node_id null = node_id(-1);
+template<typename T>
+using bbox_func = std::function<bbox(T&)>;
 
 struct node {
-    node_id children[2][2] {
-        {null, null},
-        {null, null}
-    };
+    node_id nw = null;
+    node_id ne = null;
+    node_id sw = null;
+    node_id se = null;
 };
 
-struct quadtree {
-    box bbox;
+struct qtree {
+    bbox bb;
     node_id root;
+    
+    // per node data
     std::vector<node> nodes;
-    std::vector<point> points; // variable size
-    std::vector<std::uint32_t> node_points_begin; // same size as nodes
+    std::vector<bbox> node_bbs;
+    std::vector<std::uint32_t> node_points_begin;
+
+    // per point data
+    std::vector<pointbox> pointboxes;
+    std::vector<std::uint32_t> indices;
+
+    // region query
+    void query(bbox query_bb, std::vector<std::uint32_t> &result);
+
+private:
+    void query_helper(bbox query_bb, node_id nid, std::vector<std::uint32_t> &result);
 };
 
-node_id build_impl(quadtree &tree, box const &bbox, point *begin, point *end, std::size_t depth_limit) {
-    
-    if (begin == end) return null;
+void qtree::query_helper(bbox query_bb, node_id nid, std::vector<std::uint32_t> &result) {
+    if (query_bb.intersect(node_bbs[nid])) {
+
+        bool is_not_leaf = false;
+        if (null != nodes[nid].nw && (is_not_leaf = true)) query_helper(query_bb, nodes[nid].nw, result);
+        if (null != nodes[nid].ne && (is_not_leaf = true)) query_helper(query_bb, nodes[nid].ne, result);
+        if (null != nodes[nid].sw && (is_not_leaf = true)) query_helper(query_bb, nodes[nid].sw, result);
+        if (null != nodes[nid].se && (is_not_leaf = true)) query_helper(query_bb, nodes[nid].se, result);
+
+        if (!is_not_leaf) {
+            auto i_begin = node_points_begin[nid];
+            auto i_end = node_points_begin[nid + 1];
+
+            // check intersections with pointboxes
+            for (auto i = i_begin; i != i_end; i++) {
+                if (query_bb.intersect(pointboxes[i].bb))
+                    result.push_back(indices[i]);
+            }
+
+            DrawRectangleLines( // ONLY USED FOR DEMO
+            node_bbs[nid].minx + 0.5f,
+            node_bbs[nid].miny + 0.5f,
+            node_bbs[nid].maxx - node_bbs[nid].minx + 0.5f,
+            node_bbs[nid].maxy - node_bbs[nid].miny + 0.5f,
+            BLUE
+        );
+        } else {
+            DrawRectangleLines( // ONLY USED FOR DEMO
+            node_bbs[nid].minx + 0.5f,
+            node_bbs[nid].miny + 0.5f,
+            node_bbs[nid].maxx - node_bbs[nid].minx + 0.5f,
+            node_bbs[nid].maxy - node_bbs[nid].miny + 0.5f,
+            RED
+        );
+        }
+    }
+}
+
+void qtree::query(bbox query_bb, std::vector<std::uint32_t> &result) {
+    // check overlap with root node bb
+    if (query_bb.intersect(node_bbs[root]))
+        query_helper(query_bb, root, result);
+}
+
+node_id build_helper(qtree &tree, bbox const &bb, pointbox *p_begin, pointbox *p_end, std::uint32_t depth_limit) {
+
+    if (p_begin == p_end) return null;
 
     node_id result = tree.nodes.size();
     tree.nodes.emplace_back();
-    tree.node_points_begin.push_back(begin - &tree.points.front());
+    
+    std::uint32_t idx = p_begin - &tree.pointboxes.front();
+    assert(idx >= 0);
+    assert(idx < tree.pointboxes.size());
+    tree.node_points_begin.push_back(idx);
 
-    if (begin + 1 == end || 0 == depth_limit) return result;
+    // compute bounding box for node
+    bbox node_bb = bbox();
+    std::for_each(p_begin, p_end, [&node_bb](pointbox &pb) mutable {node_bb |= pb.bb;});
+    tree.node_bbs.push_back(node_bb);
 
-    point center = middle(bbox.min, bbox.max);
+    if (p_begin + 1 == p_end || 0 == depth_limit) return result;
 
-    auto bottom = [center](point const &p){return p.y < center.y;};
-    auto left = [center](point const &p){return p.x < center.x;};
+    bbox bb_nw, bb_ne, bb_sw, bb_se;
+    bb.split_4(bb_nw, bb_ne, bb_sw, bb_se);
 
-    point *split_y = std::partition(begin, end, bottom);
-    point *split_x_lower = std::partition(begin, split_y, left);
-    point *split_x_upper = std::partition(split_y, end, left);
+    point mid = {bb_nw.maxx, bb_nw.maxy};
+    auto is_top = [mid](pointbox const &p){return p.mid.y < mid.y;};
+    auto is_left = [mid](pointbox const &p){return p.mid.x < mid.x;};
 
-    tree.nodes[result].children[0][0] = build_impl(tree, {bbox.min, center}, begin, split_x_lower, depth_limit - 1);
-    tree.nodes[result].children[0][1] = build_impl(tree, {{center.x, bbox.min.y}, {bbox.max.x, center.y}}, split_x_lower, split_y, depth_limit - 1);
-    tree.nodes[result].children[1][0] = build_impl(tree, {{bbox.min.x, center.y}, {center.x, bbox.max.y}}, split_y, split_x_upper, depth_limit - 1);
-    tree.nodes[result].children[1][1] = build_impl(tree, {center, bbox.max}, split_x_upper, end, depth_limit - 1);
+    pointbox *split_y = std::partition(p_begin, p_end, is_top);
+    pointbox *split_x_upper = std::partition(p_begin, split_y, is_left);
+    pointbox *split_x_lower = std::partition(split_y, p_end, is_left);
+
+    tree.nodes[result].nw = build_helper(tree, bb_nw, p_begin, split_x_upper, depth_limit - 1);
+    tree.nodes[result].ne = build_helper(tree, bb_ne, split_x_upper, split_y, depth_limit - 1);
+    tree.nodes[result].sw = build_helper(tree, bb_sw, split_y, split_x_lower, depth_limit - 1);
+    tree.nodes[result].se = build_helper(tree, bb_se, split_x_lower, p_end, depth_limit - 1);
 
     return result;
 }
 
-static constexpr std::size_t MAX_QUADTREE_DETPH = 5;
+static constexpr std::uint32_t MAX_QUADTREE_DEPTH = 3;
 
-quadtree build(std::vector<point> points) {
-    assert(points.size() > 0);
-    quadtree result;
-    result.points = std::move(points);
-    result.root = build_impl(
-        result,
-        bbox(&result.points.front(), &result.points.back()),
-        &result.points.front(),
-        &result.points.back(),
-        MAX_QUADTREE_DETPH
-    );
-    result.node_points_begin.push_back(result.points.size());
-    return result;
+template<typename T>
+qtree build(std::vector<T> xs, std::vector<std::uint32_t> &indices, bbox_func<T> to_bbox) {
+
+    assert(xs.size() > 0);
+
+    auto to_pointbox = [to_bbox](T x){
+        bbox bb = to_bbox(x);
+        return (pointbox){
+            {(bb.minx + bb.maxx) / 2.f, (bb.miny + bb.maxy) / 2.f},
+            bb
+        };
+    };
+
+    // convert type to bounding boxes and points
+    std::vector<pointbox> pbs(xs.size());
+    std::transform(&xs.front(), &xs.back(), &pbs.front(), to_pointbox);
+
+    // compute bounding box of midpoints
+    bbox big_box = bbox();
+    std::for_each(&pbs.front(), &pbs.back(), [&big_box](pointbox &pb) mutable {big_box |= pb.mid;});
+
+    qtree qt;
+    qt.pointboxes = std::move(pbs);
+    qt.indices = std::move(indices);
+    qt.root = build_helper(qt, big_box, &qt.pointboxes.front(), &qt.pointboxes.back(), MAX_QUADTREE_DEPTH);
+
+    qt.node_points_begin.push_back(qt.pointboxes.size());
+
+    return qt;
 }
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -4,44 +4,90 @@
 #include "raylib.h"
 #include "quadtree.h"
 
-void draw_quadtree_impl(quadtree &qt, node_id nid, box const &bbox) {
-    // draw bbox
+void draw_quadtree_impl(qtree &qt, node_id nid, bbox const &bb) {
+    // draw quadtree box
     DrawRectangleLines(
-        (int)bbox.min.x,
-        (int)bbox.min.y,
-        (int)(bbox.max.x - bbox.min.x),
-        (int)(bbox.max.y - bbox.min.y),
-        LIGHTGRAY
+        bb.minx + 0.5f,
+        bb.miny + 0.5f,
+        (bb.maxx - bb.minx) + 0.5f,
+        (bb.maxy - bb.miny) + 0.5f,
+        GRAY
     );
 
     // split recursively if there are children, draw points otherwise
-    point center = middle(bbox.min, bbox.max);
-    if (qt.nodes[nid].children[0][0] != null) draw_quadtree_impl(qt, qt.nodes[nid].children[0][0], {bbox.min, center});
-    if (qt.nodes[nid].children[0][1] != null) draw_quadtree_impl(qt, qt.nodes[nid].children[0][1], {{center.x, bbox.min.y}, {bbox.max.x, center.y}});
-    if (qt.nodes[nid].children[1][0] != null) draw_quadtree_impl(qt, qt.nodes[nid].children[1][0], {{bbox.min.x, center.y}, {center.x, bbox.max.y}});
-    if (qt.nodes[nid].children[1][1] != null) draw_quadtree_impl(qt, qt.nodes[nid].children[1][1], {center, bbox.max});
+    bbox bb_nw, bb_ne, bb_sw, bb_se;
+    bb.split_4(bb_nw, bb_ne, bb_sw, bb_se);
 
-    if (qt.nodes[nid].children[0][0] == null &&
-        qt.nodes[nid].children[0][1] == null &&
-        qt.nodes[nid].children[1][0] == null &&
-        qt.nodes[nid].children[1][1] == null){
+    if (qt.nodes[nid].nw != null) draw_quadtree_impl(qt, qt.nodes[nid].nw, bb_nw);
+    if (qt.nodes[nid].ne != null) draw_quadtree_impl(qt, qt.nodes[nid].ne, bb_ne);
+    if (qt.nodes[nid].sw != null) draw_quadtree_impl(qt, qt.nodes[nid].sw, bb_sw);
+    if (qt.nodes[nid].se != null) draw_quadtree_impl(qt, qt.nodes[nid].se, bb_se);
+
+    if (qt.nodes[nid].nw == null &&
+        qt.nodes[nid].ne == null &&
+        qt.nodes[nid].sw == null &&
+        qt.nodes[nid].se == null){
         auto start_inc = qt.node_points_begin[nid];
         auto end_excl = qt.node_points_begin[nid + 1];
         for (auto i = start_inc; i < end_excl; i++)
-            DrawCircle(qt.points[i].x, qt.points[i].y, 2.f, PINK);
+            DrawRectangle(
+                qt.pointboxes[i].bb.minx + 0.5f,
+                qt.pointboxes[i].bb.miny + 0.5f,
+                qt.pointboxes[i].bb.maxx - qt.pointboxes[i].bb.minx + 0.5f,
+                qt.pointboxes[i].bb.maxy - qt.pointboxes[i].bb.miny + 0.5f,
+                LIGHTGRAY
+            );
     }
 }
 
-void draw_quadtree(quadtree &qt) {
-    draw_quadtree_impl(qt, qt.root, bbox(&qt.points[0], &qt.points[qt.points.size()]));
+void draw_quadtree(qtree &qt) {
+    bbox big_box = bbox();
+    std::for_each(
+        &qt.pointboxes.front(),
+        &qt.pointboxes.back(),
+        [&big_box](pointbox &pb) mutable {big_box |= pb.mid;}
+    );
+    draw_quadtree_impl(qt, qt.root, big_box);
 }
 
-quadtree _qt;
+qtree _qt;
+std::vector<Rectangle> rects;
+std::vector<std::uint32_t> indices;
+
+void draw_query() {
+
+    static constexpr bbox bb = {
+        -8.0f, -8.0f, 8.0f, 8.0f
+    };
+
+    auto mpos = GetMousePosition();
+
+    // highlight query results
+    static std::vector<std::uint32_t> query_results;
+    bbox offset_bb = {
+        mpos.x + bb.minx, mpos.y + bb.miny, mpos.x + bb.maxx, mpos.y + bb.maxy
+    };
+    query_results.clear();
+    _qt.query(offset_bb, query_results);
+
+    for (auto i = 0U; i < query_results.size(); i++)
+        DrawRectangle(
+            _qt.pointboxes[query_results[i]].bb.minx + 0.5f,
+            _qt.pointboxes[query_results[i]].bb.miny + 0.5f,
+            _qt.pointboxes[query_results[i]].bb.maxx - _qt.pointboxes[query_results[i]].bb.minx + 0.5f,
+            _qt.pointboxes[query_results[i]].bb.maxy - _qt.pointboxes[query_results[i]].bb.miny + 0.5f,
+            ORANGE
+        );
+    
+    // draw box at cursor
+    DrawRectangle(mpos.x + bb.minx, mpos.y + bb.miny, bb.maxx - bb.minx, bb.maxy - bb.miny, GREEN);
+}
 
 void update_draw_frame() {
     BeginDrawing();
     ClearBackground(DARKGRAY);
     draw_quadtree(_qt);
+    draw_query();
     EndDrawing();
 }
 
@@ -51,19 +97,28 @@ int main(int argc, char **argv) {
 
     InitWindow(screen_width, screen_height, "quadtree");
 
-    // generate a bunch of points and build quadtree
+    // generate a bunch of boxes and build a quadtree
     SetRandomSeed(1);
 
-    std::vector<point> points;
-    points.reserve(100U);
+    rects.reserve(100U);
+    indices.reserve(100U);
     for (auto i = 0U; i < 100U; i++) {
-        float x, y;
-        x = (float)GetRandomValue(80, 560);
-        y = (float)GetRandomValue(60, 420);
-        points.push_back({x, y});
+        rects.emplace_back();
+        rects[i].x = (float)GetRandomValue(80 + 12, 560 - 12);
+        rects[i].y = (float)GetRandomValue(60 + 12, 420 - 12);
+        rects[i].width = (float)GetRandomValue(8, 16);
+        rects[i].height = (float)GetRandomValue(8, 16);
+        indices.push_back(i);
     }
 
-    _qt = build(points);
+    _qt = build<Rectangle>(rects, indices, [](Rectangle &r){
+        bbox bb;
+        bb.minx = r.x;
+        bb.miny = r.y;
+        bb.maxx = r.x + r.width;
+        bb.maxy = r.y + r.height;
+        return bb;
+    });
 
 #if defined(PLATFORM_WEB)
     emscripten_set_main_loop(update_draw_frame, 0, 1);
